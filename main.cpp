49b059dbdaeff36c3048a1d42a5886dfb636f689
Luna Huang <lunahuang@weintek.com>
2022-04-09 10:58:07 +0800

    Update test case

--- main.cpp
+++ main.cpp
@@ -9,59 +9,21 @@
 #include <gtest/gtest.h>
 #endif
 
-bool is_number(const std::string &str)
+std::string longest_nap_function(std::istream &is, int num)
 {
-    for (char const &c : str) {
-        if (std::isdigit(c) == 0) return false;
-    }
-    return true;
-}
+	std::string nap;
 
-void tokenize(const std::string &str, const char* delim,
-            std::vector<int> &out, int count)
-{
-    char *token = strtok(const_cast<char*>(str.c_str()), delim);
-    while (token != nullptr)
-    {
-        out.push_back(std::stoi(std::string(token)));
-        token = strtok(nullptr, delim);
-		count --;
-		if(count == 0)
-			break;
-    }
-}
-
-unsigned int add_all_function(const std::string &input, int count)
-{	
-	const char* delim = " ";
-	std::vector<int> out;
-    tokenize(input, delim, out, count);
-
-	unsigned int total = 0;
-	unsigned int total_cost = 0;
-	while(out.size() > 1){
-		std::sort(out.begin(), out.end(), std::less<int>());
-		total = out[0] + out[1];
-		out.erase(out.begin(),out.begin()+2);
-		out.push_back(total);
-		total_cost = total_cost + total;
-    }
-    return total_cost;
+	return nap;
 }
 
 void solve_uva_problem(std::istream &is, std::ostream &os)
 {
 	while(1){
-		std::string input;
-		std::getline(is, input);
-		if ((input == "0") || !is_number(input))
-			return;
-
-		int amount = std::stoi(input);
-		std::getline(is, input);
-
-		unsigned int cost = add_all_function(input, amount);
-		os << cost << std::endl;
+		std::string line;
+		std::getline(is, line);
+        int line_num = std::stoi(line);
+		std::string nap = longest_nap_function(is, line_num);
+		os << nap << std::endl;
 	}
 }
 
